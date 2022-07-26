392a9b7d91e8821ce28d6a8a1ee243484e464c2a
VigneshG575 <gvignesheducation@gmail.com>
2022-07-26 07:44:10 +0530

    morning changes

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,6 +1,26 @@
-const hamburger = document.querySelector(".hamburger");
-const navbar  = document.querySelector("nav");
+window.addEventListener("scroll",reveal);
+function reveal()
+{
+    var reveals = document.querySelectorAll(".reveal");
 
-hamburger.addEventListener('click',()=>{
-    navbar.classList.toggle('slide');
-});
+    for (var i = 0; i < reveals.length; i++)
+    {
+        var windowheight= window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint)
+        {
+            reveals[i].classList.add('active');
+        }
+
+        else
+        {
+            reveals[i].classList.remove('active');
+
+
+        }
+}
+
+
+}
